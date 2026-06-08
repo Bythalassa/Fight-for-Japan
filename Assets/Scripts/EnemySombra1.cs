04dc95f0e29757808abeffa90716429d1853cd36
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-06-08 06:07:40 -0500

    add enemy ia start

--- Assets/Scripts/EnemySombra1.cs
+++ Assets/Scripts/EnemySombra1.cs
@@ -4,6 +4,10 @@ public class EnemySombra : MonoBehaviour
 {
     public float vida = 100f;
 
+    public float MaxTime = 6;
+    public float currentTime;
+
+
     public void RecibirDamage(float damage)
     {
         vida -= damage;
@@ -13,4 +17,22 @@ public class EnemySombra : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
+    // spawner que tiene que ver con hacer el sprite invisible
+    // y luego visible con un cooldown de 6 secs
+
+    public void TimerToDoSmt()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= MaxTime)
+        {
+            //-> sprite be invisible
+            
+
+            currentTime = 0;
+        }
+    }
+
+
 }

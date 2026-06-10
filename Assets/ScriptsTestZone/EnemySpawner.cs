9629587a0fe647ea5c66bf2a9deb0f91ad186404
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-06-10 17:47:07 -0500

    testeo-spawners-testzoneEscene

--- Assets/ScriptsTestZone/EnemySpawner.cs
+++ Assets/ScriptsTestZone/EnemySpawner.cs
@@ -1,8 +1,10 @@
-/*
+
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    //direccion y vectores es la base del spawner
+
 
     public GameObject EnemyPrefab;
     public float spawnTimer = 3f;
@@ -10,13 +12,13 @@ public class EnemySpawner : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         TimerMechanic();
-        
+
     }
 
     public void TimerMechanic()
@@ -25,7 +27,7 @@ public class EnemySpawner : MonoBehaviour
         if (spawnTimer < 0)
         {
             EnemySpawner(); //esto esta llamando a otro scriptt
-            spawnTimer = 3;
+            spawnTimer = 3.2f;
 
         }
 
@@ -35,15 +37,13 @@ public class EnemySpawner : MonoBehaviour
     {
         Vector3 randomDir = new Vector3(Random.Range(-1.1f), Random.Range(-1, 1f), 0).normalized;
 
-        Vector3 fullLengthDir = randomDir * Random.Range(0f,range);
+        Vector3 fullLengthDir = randomDir * Random.Range(0f, range);
 
         GameObject enemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
 
-        enemy.transform.position += fullLengthDir; 
-       
+        enemy.transform.position += fullLengthDir;
+
     }
 
 
-
 }
-*/
